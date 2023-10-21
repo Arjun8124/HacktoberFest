3c62138b8e98741ba68e9def2e04efb0049c8ac5
Arjun Gupta <146109559+Arjun8124@users.noreply.github.com>
2023-10-21 15:14:00 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+  public static void main(Strings[] args){
+    Scanner read=new Scanner (System.in);
+    int a=read.nextInt();
+    if(a%2==0){
+      System.out.println("The number is EVEN");
+    }
+    else{
+      System.out.println("The number is ODD");
+    }
+  }
